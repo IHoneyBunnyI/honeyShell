bd67f56d5dac9e2a9fd3600a6d91e0ab6ce1614f
Reinaldo Vernon <rvernon@at-i3.msk.21-school.ru>
2021-03-23 19:12:03 +0300

    env add, нужно отключить буффер, для считывания каждой клавиши

--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,47 @@
 /*   By: rvernon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 14:04:46 by rvernon           #+#    #+#             */
-/*   Updated: 2021/03/09 14:33:20 by rvernon          ###   ########.fr       */
+/*   Updated: 2021/03/23 19:07:37 by rvernon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		main(void)
+int		env(char **s)
 {
-	//printf("%d\n", ft_atoi("100500"));
-	ft_printf("!@#");
+	int i;
 
+	i = -1;
+	while (s[++i])
+	{
+		printf("%s\n", s[i]);
+	}
+	return (0);
+}
+
+void	loop(char **e)
+{
+	int read;
+	char *line = 0;
+
+	read = 1;
+	while (read)
+	{
+		write(2, "Баш коронный, шел похоронный $: ", ft_strlen("Баш коронный, шел похоронный $: "));
+		read = get_next_line(0, &line);
+		printf("%s\n", line);
+		if ((ft_strcmp(line, "env")) == 0)
+		{
+			env(e);
+		}
+		free(line);
+	}
+}
+
+int		main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+	(void)env;
+	loop(env);
 }
