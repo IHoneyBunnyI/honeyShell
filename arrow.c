b6ee087da3d88f4a6ed8bc99da6fdea94cf252bf
Mabelle Chaya <mchaya@ga-g3.msk.21-school.ru>
2021-03-25 18:12:26 +0300

    нужно порешать tigetstr

--- arrow.c
+++ arrow.c
@@ -6,7 +6,7 @@
 /*   By: mchaya <mchaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 14:33:09 by mchaya            #+#    #+#             */
-/*   Updated: 2021/03/25 15:41:47 by mchaya           ###   ########.fr       */
+/*   Updated: 2021/03/25 17:59:59 by mchaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,11 @@
 #include <curses.h>
 #include <sys/ioctl.h>
 #include <stdlib.h>
+#include "libft.h"
 
 int ft_putint(int c)
 {
 	return (write(1, &c, 1));
-}
-
-int ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
 }
 
 int ft_putstr(char *c)
@@ -43,11 +34,7 @@ int main()
 {
 	struct termios new;
 	char c[500];
-	char *buf[2048];
 	int n;
-	char *str_up;
-	char *str_down;
-	char *str_bs;
 
 	tcgetattr(0, &new);
 	new.c_lflag &= ~(ECHO);
@@ -56,11 +43,9 @@ int main()
 	tgetent(0, getenv("TERM"));
 	ft_putstr(tgetstr("ks", 0));
 	ft_putstr(tgetstr("im", 0));
-	str_up = tgetstr("ku", 0);
-	str_down = tgetstr("kd", 0);
-	str_bs = tgetstr("kD", 0);
-
-	while (strcmp(c, "\4"))
+	printf("%s\n", tigetstr("ed"));
+	printf("%s\n", tgetstr("dm", 0));
+	while (ft_strcmp(c, "\4"))
 	{
 		tputs(save_cursor, 1, ft_putint);
 		while (1)
@@ -68,26 +53,30 @@ int main()
 //			ioctl(0, FIONREAD, &n);
 			int r = read(0, c, 100);
 			c[r] = 0;
-			if (!strcmp(c, str_up))
+			if (!ft_strcmp(c, tgetstr("ku", 0)))
 			{
 				tputs(restore_cursor, 1, ft_putint);
 				tputs(tigetstr("ed"), 1, ft_putint);
 				write(1, "previous", 8);
 			}
-			else if (!strcmp(c, str_down))
+			else if (!ft_strcmp(c, tgetstr("kd", 0)))
 			{
 				tputs(restore_cursor, 1, ft_putint);
 				tputs(tigetstr("ed"), 1, ft_putint);
 				write(1, "next", 4);
 			}
-			else if (!strcmp(c, str_bs)/* || !strcmp(c, "\177")*/)
+			else if (!ft_strcmp(c, "\177"))
 			{
 				tputs(cursor_left, 1, ft_putint);
 				tputs(tgetstr("dc", 0), 1, ft_putint);
 			}
+			else if (!ft_strcmp(c, tgetstr("kl", 0)))
+				tputs(cursor_left, 1, ft_putint);
+			else if (!ft_strcmp(c, tgetstr("kr", 0)))
+				tputs(cursor_right, 1, ft_putint);
 			else
 				write(1, c, r);
-			if (!strcmp(c, "\n") || !strcmp(c, "\4"))
+			if (!ft_strcmp(c, "\n") || !ft_strcmp(c, "\4"))
 				break;
 		}
 	}
