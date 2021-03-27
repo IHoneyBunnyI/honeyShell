02a01085adcbf61993d18975a966f8a6e5f0c339
Mabelle Chaya <mchaya@vo-l7.msk.21-school.ru>
2021-03-27 17:05:40 +0300

    история почти готова

--- srcs/arrow.c
+++ srcs/arrow.c
@@ -6,7 +6,7 @@
 /*   By: mchaya <mchaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 14:33:09 by mchaya            #+#    #+#             */
-/*   Updated: 2021/03/26 18:44:15 by mchaya           ###   ########.fr       */
+/*   Updated: 2021/03/27 16:58:57 by mchaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void	exit_terminal(void)
 int main() 
 {
 	struct termios new;
+	struct termios old;
 	char c[5];
 	char *buf;
 	int i;
@@ -41,15 +42,16 @@ int main()
 
 	i = 0;
 	n = 0;
-	tcgetattr(0, &new);
+	tcgetattr(0, &old);
+	new = old;
 	new.c_lflag &= ~(ECHO);
 	new.c_lflag &= ~(ICANON);
 	tcsetattr(0, TCSANOW, &new);
 	tgetent(0, getenv("TERM"));
-	tputs(save_cursor, 1, ft_putint);
-	printf("%s\n", tgetstr("ks", 0));
-	tputs(restore_cursor, 1, ft_putint);
-	tputs(tigetstr("ed"), 1, ft_putint);
+	setvbuf(stdout,NULL,_IONBF,0);
+	printf("%s", tgetstr("ks", 0));
+//	tputs(restore_cursor, 1, ft_putint);
+//	tputs(tigetstr("ed"), 1, ft_putint);
 	printf("%s", tgetstr("im", 0));
 	buf = malloc(4000 * 1000);
 	while (ft_strcmp(c, "\4"))
@@ -66,24 +68,20 @@ int main()
 			if (!ft_strcmp(c, tgetstr("ku", 0)))
 			{
 				tputs(restore_cursor, 1, ft_putint);
-				while (l)
+				tputs(tgetstr("dl", 0), 1, ft_putint);
+				if (i)
 				{
-					l--;
-					tputs(tgetstr("dc", 0), 1, ft_putint);
+					i--;
+					write(1, buf + i * 4000, ft_strlen(buf + i * 4000));
+					n = ft_strlen(buf + i * 4000);
 				}
-				i--;
-				write(1, buf + i * 4000, ft_strlen(buf + i * 4000));
-				n = ft_strlen(buf + i * 4000);
 			}
 			else if (!ft_strcmp(c, tgetstr("kd", 0)))
 			{
 				tputs(restore_cursor, 1, ft_putint);
-				while (l)
-				{
-					tputs(tgetstr("dc", 0), 1, ft_putint);
-					l--;
-				}
-				write(1, "next", 4);
+				tputs(tgetstr("dl", 0), 1, ft_putint);
+				i++;
+				write(1, buf + i * 4000, ft_strlen(buf + i * 4000));
 				n = 4;
 			}
 			else if (!ft_strcmp(c, "\177"))
@@ -96,27 +94,41 @@ int main()
 				}
 			}
 			else if (!ft_strcmp(c, tgetstr("kl", 0)))
-				tputs(cursor_left, 1, ft_putint);
+			{
+				while (n)
+				{
+					tputs(cursor_left, 1, ft_putint);
+					n--;
+				}
+			}
 			else if (!ft_strcmp(c, tgetstr("kr", 0)))
 			{
-				tputs(cursor_right, 1, ft_putint);
-				n++;
+				int k = n;
+				 (k != n)
+				{
+					tputs(cursor_right, 1, ft_putint);
+					k++;
+				}
 			}
 			else
 			{
 				write(1, c, r);
-				buf[i * 4000 + n++] = *c;
+				if (ft_strcmp(c, "\n"))
+					buf[i * 4000 + n++] = *c;
 			}
 			if (!ft_strcmp(c, "\n") || !ft_strcmp(c, "\4"))
 			{
 				buf[i * 4000 + n] = 0;
-				i++;
+				if (ft_strcmp(buf + (i * 4000), "\n"))
+					i++;
 				n = 0;
+				buf[i * 4000] = 0;
 				break;
 			}
 		}
 	}
-	printf("%s", tgetstr("ke", 0));
+//	printf("%s", tgetstr("ke", 0));
+	tcsetattr(0, TCSANOW, &old);
 	printf("\n");
 	return 0;
 }
