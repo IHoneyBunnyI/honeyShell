1114dfec4e261dad70a5c6236b1c67738c88523d
Касим Байбиков <kasim.baybikov@gmail.com>
2021-04-02 22:58:52 +0300

    bug av and env fixed

--- srcs/av_env_copy.c
+++ srcs/av_env_copy.c
@@ -6,33 +6,34 @@
 /*   By: rvernon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 15:13:30 by rvernon           #+#    #+#             */
-/*   Updated: 2021/04/02 15:29:19 by rvernon          ###   ########.fr       */
+/*   Updated: 2021/04/02 22:58:10 by kasimbayb        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-void	param_copy(char **to, char **from)
+void	param_copy(t_all *all, char **av, char **env)
 {
 	int i;
 	
 	i = 0;
-	(void)to;
-	while (from[i])
-	{
-		printf("%s\n", from[i]);
+	while (av[i])
 		i++;
-	}
-	/*while (from[i])*/
-		/*i++;*/
-	/*if (!(to = malloc(sizeof(char *) * (i + 1))))*/
-		/*error(1);*/
-	/*to[i] = 0;*/
-	/*while (i--)*/
-		/*to[i] = ft_strdup(from[i]);*/
+	if (!(all->av = malloc(sizeof(char *) * (i + 1))))
+		error(1);
+	all->av[i] = 0;
+	while (i--)
+		all->av[i] = ft_strdup(av[i]);
+	i = 0;
+	while (env[i])
+		i++;
+	if (!(all->env = malloc(sizeof(char *) * (i + 1))))
+		error(1);
+	all->env[i] = 0;
+	while (i--)
+		all->env[i] = ft_strdup(env[i]);
 }
 
 void	av_env_copy(t_all *all, char **av, char **env)
 {
-	param_copy(all->env, env);
-	param_copy(all->av, av);
+	param_copy(all, av, env);
 }
