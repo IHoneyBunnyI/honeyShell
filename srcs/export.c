9b3b96effb0cf4f665245131d607bb84cbec6836
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-04-07 18:09:28 +0300

    export is done, парсится строка, добовляется и сделана загатовка под копирования в env, также надо проверить влидность параметров

--- srcs/export.c
+++ srcs/export.c
@@ -6,7 +6,7 @@
 /*   By: rvernon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 19:01:00 by rvernon           #+#    #+#             */
-/*   Updated: 2021/04/05 20:31:09 by rvernon          ###   ########.fr       */
+/*   Updated: 2021/04/07 18:08:37 by rvernon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,14 +57,30 @@ void	free_split(char **split)
 	free(split);
 }
 
+void	copy_args(t_all *all, char **new_env, char **arg)
+{
+	*new_env = ft_strdup(*arg);
+	(void)all;
+}
+
 void	add_sort_env(t_all *all, char **args)
 {
 	int i;
 	int j;
+	char **new_env;
 
 	i = num_of_split(all->sort_env);
 	j = num_of_split(args);
-	printf("%d %d\n", j, i);
+	new_env = malloc(sizeof(char *) * (i + j + 1));
+	new_env[i + j] = 0;
+	j = -1;
+	i = -1;
+	while (args[++j])
+		copy_args(all, &new_env[j], &args[j]);
+	while (all->sort_env[++i])
+		new_env[j + i] = all->sort_env[i];
+	free(all->sort_env);
+	all->sort_env = new_env;
 }
 
 void	export(t_all *all, char **env, char **args)
