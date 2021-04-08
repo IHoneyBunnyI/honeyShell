f9cb6b7ed016d81742049df4166c72175f7dce96
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-04-08 19:41:01 +0300

    исправил баг с сортировкой енв, но теперь при экспорте утечка

--- srcs/print_export.c
+++ srcs/print_export.c
@@ -6,7 +6,7 @@
 /*   By: rvernon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 18:44:17 by rvernon           #+#    #+#             */
-/*   Updated: 2021/04/08 19:04:40 by rvernon          ###   ########.fr       */
+/*   Updated: 2021/04/08 19:40:26 by rvernon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,16 +37,35 @@ char **sort_env(char **env)
 	return (env);
 }
 
+char **copy_env(char **env)
+{
+	int i;
+	char **for_sort;
+
+	i = 0;
+	while (env[i])
+		i++;
+	if (!(for_sort = malloc(sizeof(char *) * (i + 1))))
+		exit(0);
+	for_sort[i] = 0;
+	while (i--)
+	{
+		for_sort[i] = env[i];
+	}
+	return (for_sort);
+}
+
 void	print_export(char **env)
 {
 	int i;
 	char **print_env;
 
 	i = 0;
-	print_env = sort_env(env);
-	while (env[i])
+	print_env = copy_env(env);
+	print_env = sort_env(print_env);
+	while (print_env[i])
 	{
-		printf("declare -x %s\"\n", env[i]);
+		printf("declare -x %s\"\n", print_env[i]);
 		i++;
 	}
 }
