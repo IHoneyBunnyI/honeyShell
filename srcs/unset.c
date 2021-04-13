a26551315529131a2d49a91c9f54ce51632b4787
Касим Байбиков <kasim.baybikov@gmail.com>
2021-04-13 21:02:43 +0300

    mkdir objs

--- srcs/unset.c
+++ srcs/unset.c
@@ -6,19 +6,30 @@
 /*   By: rvernon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 17:57:21 by rvernon           #+#    #+#             */
-/*   Updated: 2021/04/08 18:30:43 by rvernon          ###   ########.fr       */
+/*   Updated: 2021/04/13 20:58:31 by kasimbayb        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	unset(char **args)
+int		find_name(t_all *all, char *arg)
+{
+	return (1);
+}
+
+void	del_arg(t_all *all, char *arg)
+{
+	;
+}
+
+void	unset(t_all *all, char **args)
 {
 	int i;
 
 	i = 0;
 	while (args[i])
 	{
-		;
+		if (find_name(all, args[i]))
+			del_arg(all, args[i]);
 	}
 }
