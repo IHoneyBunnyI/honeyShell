a9057ae95d55b399449077aabdf3e3c6c7d0fb7c
Mabelle Chaya <mchaya@at-i7.msk.21-school.ru>
2021-05-14 16:40:53 +0300

    kavichki

--- srcs/parse_dollars.c
+++ srcs/parse_dollars.c
@@ -7,13 +7,24 @@ char	*find_in_env(char *comand, char **env)
 	return (0);
 }
 
+/*void	next_char(char *str, int *j, char n)
+{
+	char	*buf;
+
+	buf = malloc(2);
+	buf[0] = n;
+	buf[1] = '\0';
+	str = ft_strjoin(str, buf);
+	(*j)++;
+	free(buf);
+}*/
+
 char	**parse_dollars(t_all *all, char **args, char **env)
 {
 	char	**ret;
 	int		i;
 	int		j;
 	char	*buf;
-	//int		is_set;
 
 	i = 0;
 	while (args[i])
@@ -24,36 +35,65 @@ char	**parse_dollars(t_all *all, char **args, char **env)
 	j = 0;
 	buf = malloc(2);
 	buf[1] = '\0';
-	while(args[i])
+	while (args[i])
 	{
 		ret[i] = malloc(1);
 		ret[i][0] = '\0';
-		while(args[i][j])
+		while (args[i][j])
 		{
-			if (args[i][j] == '$')
+			if (args[i][j] == '\'')
 			{
 				j++;
-				if (args[i][j] == 0)
-					ret[i] = ft_strjoin(ret[i], "$");
-				else if (args[i][j] == '?')
-					ret[i] = ft_strjoin(ret[i], ft_itoa(all->exit_status));
-				else
+				while (args[i][j] != '\'')
 				{
-					ret[i] = ft_strjoin(ret[i], check_env(&args[i][j],
-														  env));
-					while(skip_env(args[i][j]))
+					buf[0] = args[i][j];
+					ret[i] = ft_strjoin(ret[i], buf);
+					j++;
+				}
+
+				j++;
+			}
+			else
+			{
+				if (args[i][j] == '\"')
+					j++;
+				if (args[i][j] == '$' && args[i][j])
+				{
+					j++;
+					if (args[i][j] == 0)
+						ret[i] = ft_strjoin(ret[i], "$");
+					else if (args[i][j] == '?')
+					{
+						ret[i] = ft_strjoin(ret[i], ft_itoa(all->exit_status));
 						j++;
+					}
+					else
+					{
+						ret[i] = ft_strjoin(ret[i], check_env(&args[i][j],
+									env));
+						while (skip_env(args[i][j]))
+							j++;
+					}
+				}
+				else if (args[i][j] == '\\' && args[i][j])
+				{
+					j++;
+					buf[0] = args[i][j];
+					ret[i] = ft_strjoin(ret[i], buf);
+					j++;
+				}
+				else if (args[i][j] && args[i][j] != '\"' && args[i][j] != '\'')
+				{
+					buf[0] = args[i][j];
+					ret[i] = ft_strjoin(ret[i], buf);
+					j++;
 				}
 			}
-			if (args[i][j] == '\\')
-				j++;
-			buf[0] = args[i][j];
-			ret[i] = ft_strjoin(ret[i], buf);
-			j++;
 		}
 		i++;
 		j = 0;
 	}
+	free(buf);
 	free_split(args);
 	return (ret);
 }
