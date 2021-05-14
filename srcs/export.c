16c5124f2d2ff4c8fe372e34edd186e221022a17
IHoneyBunnyI <kasim.baybikov@gmail.com>
2021-05-14 15:45:23 +0300

    export normalno nahodit vse parametri

--- srcs/export.c
+++ srcs/export.c
@@ -23,40 +23,104 @@ int	num_of_split(char **split)
 	return (i);
 }
 
-void	free_split(char **split)
+int	arg_lenth(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] && s[i] != '=')
+		i++;
+	return (i);
+}
+
+int	ft_strstr(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s2[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (0);
+	}
+	if (s1[i] == '=')
+		return (1);
+	return (0);
+}
+
+int	find_arg_in_env(char *arg, char **args)
+{
+	int i;
+	int	arg_len;
+
+	(void)arg;
+	i = 0;
+	arg_len = 0;
+	while (args[i])
+	{
+		arg_len = arg_lenth(args[i]);
+		if (ft_strstr(args[i], arg))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	valid_arg(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (split[i])
-		free(split[i++]);
-	free(split);
+	if (ft_isdigit(s[0]) || s[0] == '=' || s[0] == '+')
+		return (0);
+	while (s[i] && s[i] != '=')
+	{
+		if (!ft_isalnum(s[i]) && s[i] != '_' && !(s[i] == '+' && s[i + 1] == '='))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-void	copy_args(t_all *all, char **new_env, char **arg)
+void	error_export(char *s)
 {
-	*new_env = ft_strdup(*arg);
-	(void)all;
+	ft_putstr_fd("🚀: export: ", 2);
+	ft_putstr_fd(s, 2);
+	ft_putendl_fd(": not a valid identifier", 2);
+}
+
+int	check_args(t_all *all, char **args)
+{
+	int i;
+	char *s;
+
+	i = -1;
+	while (args[++i])
+	{
+		s = args[i];
+		if (!valid_arg(s))
+		{
+			error_export(s);
+			continue ;
+		}
+		if (find_arg_in_env(s, all->env))
+			printf("NASHEL\n");
+	}
+	return (0);
 }
 
 void	add_sort_env(t_all *all, char **args)
 {
-	int		i;
-	int		j;
-	char	**new_env;
+	int		old;
+	int		new;
+	/*char	**new_env;*/
 
-	i = num_of_split(all->env);
-	j = num_of_split(args);
-	new_env = malloc(sizeof(char *) * (i + j + 1));
-	new_env[i + j] = 0;
-	j = -1;
-	i = -1;
-	while (args[++j])
-		copy_args(all, &new_env[j], &args[j]);
-	while (all->env[++i])
-		new_env[j + i] = all->env[i];
-	free(all->env);
-	all->env = new_env;
+	old = num_of_split(all->env);
+	new = check_args(all, args);
+	/*free(all->env);*/
+	/*all->env = new_env;*/
 }
 
 void	export(t_all *all, char **args, int fd)
--- srcs/util.c
+++ srcs/util.c
@@ -35,3 +35,13 @@ int	count_dots(char **args)
 	}
 	return (dots);
 }
+
+void	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+		free(split[i++]);
+	free(split);
+}
